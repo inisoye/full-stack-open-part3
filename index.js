cfc8c9b7a55355b86a75470424c0a68c744f090d
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-11-02 20:34:56 +0100

    Add functionality for deleting and updating persons, obtaining single person and adding errors

--- index.js
+++ index.js
@@ -4,44 +4,25 @@ const app = express();
 const Person = require('./models/person');
 const morgan = require('morgan');
 const cors = require('cors');
+const { response } = require('express');
+
+app.use(cors());
 
 app.use(express.static('build'));
+
 /* Middleware (json-parser) used in post and put requests. Explained here: 
 https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded/51844327 */
 app.use(express.json());
-app.use(cors());
 
-// Create custom morgan token
+// Create custom morgan token thar returns the body of a request
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body);
 });
+
 // Use morgan middleware with custom token created above
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 );
-
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
@@ -50,28 +31,34 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
-  );
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        persons.length
+      } people</p><p>${new Date().toString()}</p>`
+    );
+  });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((eachPerson) => eachPerson.id === id);
-
-  // Prevent undefined responses
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      // Prevent undefined responses
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((eachPerson) => eachPerson.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -98,6 +85,39 @@ called at start of script */
     response.json(savedPerson.toJSON());
   });
 });
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+// Middleware for handling requests with no known endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+// Middleware for handling errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
